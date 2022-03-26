bbc47e38dfa4bcea5d7cd5e36fbae0f566aaafae
Jakub Michalik <Jakubmichalik0000@gmail.com>
2022-03-26 17:56:48 +0100

    Create new branch "NewDrag&DropBar"

---
+++ GameAssistant/TODOList.cs
@@ -0,0 +1,15 @@
+﻿// TODO Opcja autostartu
+// TODO Animacja koloru
+// TODO Własny drag&drop bar
+// TODO Własny styl okna
+// TODO Przebudować architekturę menu
+// TODO Przycisk close w menu
+// TODO Poprawić styl programu
+// TODO Interaktywne elementy w aplikacji (wizualnie)
+
+// TODO Przechwytywanie aplikacji takich jak: Spotify, Discord
+// TODO Przechwytywanie stron internetowych takich jak: Youtube, Facebook, Messangare
+
+// TODO Nowe Widgety: kalkulator, mikser głośności, detektor klawiszy
+
+// TODO Dodać samouczek
