a9fcf80edadd18f35a2e93bf8f7af1cff52c922f
Jakub Michalik <Jakubmichalik0000@gmail.com>
2022-04-02 15:35:43 +0200

    Graphical interface.

--- GameAssistant/TODOList.cs
+++ GameAssistant/TODOList.cs
@@ -1,11 +1,15 @@
-﻿// TODO Opcja autostartu
-// TODO Animacja koloru
-// TODO Własny drag&drop bar
-// TODO Własny styl okna
-// TODO Przebudować architekturę menu
-// TODO Przycisk close w menu
-// TODO Poprawić styl programu
-// TODO Interaktywne elementy w aplikacji (wizualnie)
+﻿// TODO (OK) Własny drag&drop bar
+// TODO (OK) Własny styl okna
+
+// TODO (KK) Interaktywne elementy w aplikacji (wizualnie)
+// TODO (OK) Poprawić styl programu
+// TODO (KK) Przebudować architekturę menu
+// TODO (OK) Przycisk close w menu
+// TODO Checkbox auto startu w menu
+
+// TODO (OK) Opcja autostartu
+
+// TODO (OK) Animacja koloru
 
 // TODO Przechwytywanie aplikacji takich jak: Spotify, Discord
 // TODO Przechwytywanie stron internetowych takich jak: Youtube, Facebook, Messangare
